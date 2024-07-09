20d46114d1e9270d16270349058bcce7116bbb80
devEduSantos <129607995+devEduSantos@users.noreply.github.com>
2024-07-09 16:27:18 -0300

    09-07-2024

---
+++ index.js
@@ -0,0 +1,65 @@
+var a = document.querySelectorAll(".drum").length;
+var i = 0;
+var audio = new Audio('./sounds/tom-1.mp3');
+
+// Checking the button clicked
+while(i < a){
+document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+    var buttonInnerHTML = this.innerHTML;
+
+
+    makeSound(buttonInnerHTML)
+
+    buttonAnimation(buttonInnerHTML)
+
+    });
+    i++;
+}
+
+// checking the keyboard pressed
+document.addEventListener("keydown", function(event){
+    makeSound(event.key)
+    
+    buttonAnimation(event.key);
+})
+function makeSound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+        case "l":
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey)
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100)
+}
